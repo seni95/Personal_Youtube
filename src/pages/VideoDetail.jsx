6dbfd1efeab986d40c4ee7aeef8c192e0b75dce3
seni95 <ghkdtpdus95@naver.com>
2023-02-20 05:54:32 +0900

    http to https

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -10,7 +10,7 @@ export default function VideoDetail() {
     <section className='flex flex-col lg:flex-row'>
       <article className='basis-4/6'>
       <iframe id="player" type="text/html" width="100%" height="640"
-      src={`http://www.youtube.com/embed/${video.id}`}
+      src={`https://www.youtube.com/embed/${video.id}`}
       frameBorder="0"
       title={title}
       ></iframe>
