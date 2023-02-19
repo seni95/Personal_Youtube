ebfaee867ccdcd1821602a3cdc97064fccee2d79
seni95 <ghkdtpdus95@naver.com>
2023-02-20 05:47:35 +0900

    fix video detail routing error

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -10,7 +10,7 @@ export default function VideoCard({video, type}) {
     return (
     <li
     className={isList?'flex gap-1 m-2':''} 
-    onClick={()=>{navigate(`videos/watch/${video.id}`,{state:{video}})}}>
+    onClick={()=>{navigate(`/videos/watch/${video.id}`,{state:{video}})}}>
         <img className={isList?'w-60 mr-2':'w-full'} src={thumbnails.medium.url} alt={title} />
         <div>
             <p className='font-semibold my-2 line-clamp-2'>{title}</p>
